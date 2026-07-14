b93f0d75d83e242111214b124036f7ff56fd1158
Georgia Burke <burkege@oregonstate.edu>
2026-07-14 03:40:45 +0000

    FB COMMIT 07-13-2026 07:40 MT - Commit feature/basic_app as separate branch.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from sqlalchemy.orm import joinedload
 import os
 from dotenv import load_dotenv
 from database import db
@@ -16,7 +15,9 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -30,6 +31,8 @@ with app.app_context():
     seed_db()
 
 # Set up global logged in user based on session for templates to access
+
+
 @app.context_processor
 def set_current_user():
     user_id = session.get('user_id')
@@ -37,15 +40,18 @@ def set_current_user():
     return dict(current_user=user)
 
 # Routes
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
+
     if request.method == "POST":
 
         # Get form data
@@ -57,7 +63,8 @@ def login():
             return {"Error: Missing email or password."}, 400
 
         # Find user with specified email
-        user = db.session.scalars(db.select(User).where(User.email == email)).first()
+        user = db.session.scalars(
+            db.select(User).where(User.email == email)).first()
 
         # Validate password if user exists
         if user and user.check_password(password):
@@ -67,8 +74,11 @@ def login():
                 return redirect(url_for("admin"))
             else:
                 return redirect(url_for("database"))
-        
-        return render_template("login.html", error="Invalid email or password."), 401
+
+        return render_template(
+            "login.html", error="Invalid email or password."
+        ), 401
+
 
 @app.route("/admin")
 def admin():
@@ -77,25 +87,33 @@ def admin():
 
     if user:
         # If user was found with matching id, get their accounts and return
-        accts = db.session.scalars(db.select(Account).where(Account.user_id == user.id)).all()
-        return render_template("admin_db.html", selected_user=user, accts=accts)
+        accts = db.session.scalars(db.select(Account).where(
+            Account.user_id == user.id)).all()
+        return render_template(
+            "admin_db.html", selected_user=user, accts=accts
+        )
     else:
         if user_id:
             # Send error message that id did not match user
-            return render_template("admin_db.html", error="No user exists with that id."), 400
+            return render_template(
+                "admin_db.html", error="No user exists with that id."
+            ), 400
         else:
             # Render initial page before user id selection by admin user
             return render_template("admin_db.html")
+
 
 @app.route("/database")
 def database():
     # Can retrieve accounts data in template for global current user
     return render_template("database.html")
 
+
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
