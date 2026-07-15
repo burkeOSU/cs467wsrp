db43b0f82f2a2eb861d6b3f9404e57ff867df380
Mary Dean <mdean726@gmail.com>
2026-07-14 20:26:57 -0500

    Add account form, error based sql injection

--- app.py
+++ app.py
@@ -32,9 +32,8 @@ with app.app_context():
     db.create_all()
     seed_db()
 
+
 # Set up global logged in user based on session for templates to access
-
-
 @app.context_processor
 def set_current_user():
     user_id = session.get('user_id')
@@ -75,7 +74,7 @@ def login():
             if user.role == UserRole.ADMIN:
                 return redirect(url_for("admin"))
             else:
-                return redirect(url_for("database"))
+                return redirect(url_for("accounts"))
 
         return render_template(
             "login.html", error="Invalid email or password."
@@ -98,23 +97,61 @@ def admin():
         accts = db.session.scalars(db.select(Account).where(
             Account.user_id == user.id)).all()
         return render_template(
-            "admin_db.html", selected_user=user, accts=accts
+            "admin.html", selected_user=user, accts=accts
         )
     else:
         if user_id:
             # Send error message that id did not match user
             return render_template(
-                "admin_db.html", error="No user exists with that id."
+                "admin.html", error="No user exists with that id."
             ), 400
         else:
             # Render initial page before user id selection by admin user
-            return render_template("admin_db.html")
+            return render_template("admin.html")
 
 
-@app.route("/database")
-def database():
+@app.route("/accounts", methods=["GET"])
+def accounts():
     # Can retrieve accounts data in template for global current user
-    return render_template("database.html")
+    return render_template("accounts.html")
+
+ 
+@app.route("/new_account", methods=["GET", "POST"])
+def new_account():
+    if request.method == "GET":
+        return render_template("new_account.html")
+    
+    if request.method == "POST":
+        user_id = session.get('user_id')
+
+        name = request.form.get("name")
+        number = request.form.get("number")
+        balance = request.form.get("balance")
+
+        # Allows for SQL injection to display informational error message on screen
+        stmt = (f"INSERT INTO accounts (name, number, balance, user_id)" 
+                f"VALUES ('{name}', '{number}', {balance}, {user_id})")
+        
+        # Parameterized inputs to protect against SQL injection
+        # stmt = (f"INSERT INTO accounts (name, number, balance, user_id)" 
+        #         f"VALUES (:name, :number, :balance, :user_id)")
+        try:
+            # Insecure code
+            db.session.execute(text(stmt))
+            # Secure code
+            # db.session.execute(text(stmt), {"name": name, "number": number, "balance": balance, "user_id": user_id})
+            db.session.commit()
+            return redirect(url_for("accounts"))
+        except Exception as e:
+            db.session.rollback()
+            # Displays raw error message, giving valuable information to attacker
+            return render_template(
+               "new_account.html", error=str(e)
+            ), 500
+            # Displays safer generic message
+            # return render_template(
+            #     "new_account.html", error="An error occurred creating the new account."
+            # ), 500
 
 
 @app.route("/logout")
