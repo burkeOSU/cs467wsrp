55be348231eb62c5a2052a708029cbd96780b834
Mary Dean <mdean726@gmail.com>
2026-07-22 16:47:42 -0500

    Adds hint btns at bottom as imported component, changes security toggle from macro to imported component

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    // For security toggle buttons
     const buttons = document.querySelectorAll('.toggleBtn');
     const securitySelection = document.getElementById('securityChoice');
 
@@ -17,6 +17,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // 3. Update hidden input value to match selected button
             securitySelection.value = button.getAttribute('data-value');
+        });
+    });
+
+    // For hint/explanation buttons
+    const hintButtons = document.querySelectorAll('.hintBtn');
+    const contents = document.querySelectorAll('.content');
+
+    hintButtons.forEach(hintBtn => {
+        hintBtn.addEventListener('click', function() {
+            const target = this.getAttribute('data-target');
+            const contentTarget = document.getElementById(target);
+            // If content is already displayed, hide it
+            if (contentTarget.classList.contains('show')) {
+                contentTarget.classList.remove('show');
+                return;
+            }
+            // Otherwise hide all contents
+            contents.forEach(content => content.classList.remove('show'));
+            // Show only the targeted panel
+            contentTarget.classList.add('show');
         });
     });
 });
