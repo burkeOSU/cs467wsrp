71cefab5323e09e310a8f595c0103aab3f065c0f
Mary Dean <mdean726@gmail.com>
2026-07-22 09:27:02 -0500

    Adds security toggle for new account, separates route function into helpers

--- app.py
+++ app.py
@@ -40,9 +40,48 @@ def set_current_user():
     user = db.session.get(User, user_id) if user_id else None
     return dict(current_user=user)
 
+# Helper Functions
+def create_account_secure(account_data):
+    # Parameterized inputs to protect against SQL injection
+    stmt = (f"INSERT INTO accounts (name, number, balance, user_id)"
+            f"VALUES (:name, :number, :balance, :user_id)")
+    try:
+        # Secure submitting parameterized data
+        db.session.execute(text(stmt), {
+            "name": account_data["name"],
+            "number": account_data["number"],
+            "balance": account_data["balance"],
+            "user_id": account_data["user_id"]
+        })
+        db.session.commit()
+        return redirect(url_for("accounts"))
+    except Exception as e:
+        db.session.rollback()
+        # Displays safer generic message instead of raw error msg
+        return render_template(
+            "new_account.html", error="An error occurred creating the new account."
+        ), 500
+
+
+def create_account_insecure(account_data):
+    # Allows for SQL injection
+    stmt = (f"INSERT INTO accounts (name, number, balance, user_id)"
+            f"VALUES ('{account_data["name"]}', '{account_data["number"]}', "
+            f"{account_data["balance"]}, {account_data["user_id"]})")
+    try:
+        # Insecure submitting concatenated raw SQL to db
+        db.session.execute(text(stmt))
+        db.session.commit()
+        return redirect(url_for("accounts"))
+    except Exception as e:
+        db.session.rollback()
+        # Displays raw error message, giving valuable information to attacker
+        return render_template(
+            "new_account.html", error=str(e)
+        ), 500
+
+
 # Routes
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -56,6 +95,7 @@ def login():
     if request.method == "POST":
 
         # Get form data
+        security_choice = request.form.get("security_choice")
         email = request.form.get("email")
         password = request.form.get("password")
 
@@ -123,42 +163,27 @@ def new_account():
         return render_template("new_account.html")
     
     if request.method == "POST":
-        user_id = session.get('user_id')
-
-        name = request.form.get("name")
-        number = request.form.get("number")
-        balance = request.form.get("balance")
+        security_choice = request.form.get("security_choice")
+        data = {
+            "user_id": session.get('user_id'),
+            "name": request.form.get("name"),
+            "number": request.form.get("number"),
+            "balance": request.form.get("balance")
+        }
 
         # Check that all attributes are present
-        if not name or not number or not balance:
+        if not data["name"] or not data["number"] or not data["balance"]:
             return render_template(
                "new_account.html", error="Missing one or more parameters."
             ), 400
 
-        # Allows for SQL injection to display informational error message on screen
-        stmt = (f"INSERT INTO accounts (name, number, balance, user_id)" 
-                f"VALUES ('{name}', '{number}', {balance}, {user_id})")
-        
-        # Parameterized inputs to protect against SQL injection
-        # stmt = (f"INSERT INTO accounts (name, number, balance, user_id)" 
-        #         f"VALUES (:name, :number, :balance, :user_id)")
-        try:
-            # Insecure code
-            db.session.execute(text(stmt))
-            # Secure code
-            # db.session.execute(text(stmt), {"name": name, "number": number, "balance": balance, "user_id": user_id})
-            db.session.commit()
-            return redirect(url_for("accounts"))
-        except Exception as e:
-            db.session.rollback()
-            # Displays raw error message, giving valuable information to attacker
-            return render_template(
-               "new_account.html", error=str(e)
-            ), 500
-            # Displays safer generic message
-            # return render_template(
-            #     "new_account.html", error="An error occurred creating the new account."
-            # ), 500
+        if security_choice == "vulnerable":
+            # Allows for SQL injection to display informational error message on screen (error-based SQLi)
+            return create_account_insecure(data)
+
+        if security_choice == "hardened":
+            # Protects against SQL injection by parameterizing inputs
+            return create_account_secure(data)
 
 
 @app.route("/register", methods=["GET", "POST"])
