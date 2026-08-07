f36c2272e84b15c3aaa8a5bf2680a67b18cb930a
Mary Dean <mdean726@gmail.com>
2026-08-07 00:17:22 -0500

    Updated readme and screenshots for mishandled exception, added back in secure register function

--- routes/register.py
+++ routes/register.py
@@ -36,7 +36,11 @@ def register():
         password_hash = generate_password_hash(password)
         role = UserRole.CUSTOMER.value
 
-        return register_insecure(email, first_name, last_name, role, password_hash)
+        if not session["toggle_misexc"] or session["toggle_misexc"] == "vulnerable":
+            return register_insecure(email, first_name, last_name, role, password_hash)
+
+        if session["toggle_misexc"] == "hardened":
+            return register_secure(email, first_name, last_name, role, password_hash)
 
 
 # Helper functions
@@ -58,3 +62,32 @@ def register_insecure(email, first_name, last_name, role, password_hash):
     session["user_id"] = user.id
     session["user_fname"] = user.first_name
     return redirect(url_for("account.accounts"))
+
+
+def register_secure(email, first_name, last_name, role, password_hash):
+    # Insecure stmt that allows for SQL injection
+    stmt = (
+        "INSERT INTO users (email, first_name, last_name, role, "
+        "password_hash)"
+        f"VALUES ('{email}', '{first_name}', '{last_name}', '{role}', "
+        f"'{password_hash}')"
+    )
+    # Try/except block to gracefully handle any exceptions
+    try:
+        # Send constructed stmt to database to register user
+        result = db.session.execute(text(stmt))
+        db.session.commit()
+        # Retrieve newly created user info to set session variables
+        new_user_id = result.lastrowid
+        user = db.session.get(User, new_user_id)
+        session["user_id"] = user.id
+        session["user_fname"] = user.first_name
+        return redirect(url_for("account.accounts"))
+    except BaseException:
+        db.session.rollback()
+        # Appropriate generic error message is displayed back to user
+        return render_template(
+            "register.html",
+            error="An error occurred creating the new user.",
+            security="hardened"
+        ), 500
